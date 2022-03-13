256bcd37976460f579a227cf6b1119ccb8a63571
stevo95 <stefansarmir@Stefans-MacBook-Pro.local>
2022-03-13 17:15:40 +0100

    edit: changed tab name

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { useEffect } from 'react';
 import './App.css';
 import GraphPage from './Pages/Graph.Page'
 
 function App() {
+
+  useEffect(() => {
+    document.title = "Algo Playground"
+  }, [])
+
+  
   return (
     <div className="App">
       <div className= "graph_canvas">
